34d59312e7eb23df6b2baae1896395792009e389
amanuel tegegne <amanuel0924@gmail.com>
2023-10-22 20:26:39 +0300

    Edditing About page

--- src/page/About.js
+++ src/page/About.js
@@ -1,9 +1,19 @@
 import React from "react"
+import classes from "./About.module.css"
 
 const About = () => {
   return (
-    <div>
-      <h2>About</h2>
+    <div className={classes.container}>
+      <div>
+        <h1 className={classes.h1}>
+          Hi, my name is <span className={classes.color}>Amanuel Tegegne</span>
+          <br />
+          Front End Developer
+          <br />
+          This is <span className={classes.color}>Yenetta</span>
+          <span /> Bootcamp Challenge
+        </h1>
+      </div>
     </div>
   )
 }
